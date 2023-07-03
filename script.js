8c3a96c37b2f7e6e9bdb1379cc0915fe429dfa4f
the-paranoid07 <121954633+the-paranoid07@users.noreply.github.com>
2023-07-03 20:30:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+
+const div = document.getElementById("browser-info");
+const nameOfBrowser=navigator.appName;
+const version = navigator.appVersion;
+
+div.innerText=`You are using ${nameOfBrowser} version ${version}`;
